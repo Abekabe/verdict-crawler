4b9596517c839abc4c6352097bf13c0093b1d664
Abekabe <ms0229643@gmail.com>
2019-06-03 16:30:22 +0800

    fix word counter problem

--- word_counter.py
+++ word_counter.py
@@ -4,9 +4,12 @@
 if __name__ == '__main__':
     try:
         while True:
-            content = input("Please enter the content: ")
+            print("Please enter the content: ")
+            stopword = ':q'
+            content = ''
+            for line in iter(input, stopword):
+                content += line
             print("Content length: " + str(len(content)))
-            #input('Please press any key to continue...')
     except KeyboardInterrupt:
         print()
         input('Please press any key to continue...')
